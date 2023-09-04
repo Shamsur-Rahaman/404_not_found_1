a267f85c77b696dae382ca572230b3d435df18c7
Shamsur-Rahaman <shamsurrahaman132@gmail.com>
2023-09-04 11:58:59 +0600

    First 404 responsive page

---
+++ git_firstTime.sh
@@ -0,0 +1,7 @@
+
+git init
+git add .
+git commit -m "First 404 responsive page"
+git branch -M main
+git remote add origin https://github.com/Shamsur-Rahaman/404_not_found_1.git
+git push -u origin main
